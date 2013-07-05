f5590562c42904439cf0a775a85cd033c187d103
yaoohfox <yaoohfox@163.com>
2013-07-05 22:39:00 +0800

    Initial Commit

---
+++ StoryDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryDemo
+//
+//  Created by jhwang on 13-7-5.
+//  Copyright (c) 2013年 jhwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryDemo
+//
+//  Created by jhwang on 13-7-5.
+//  Copyright (c) 2013年 jhwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
